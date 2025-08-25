fd2f10e853b306921e87c4be870b307d34945efd
後天 <1056917561@qq.com>
2025-08-25 09:05:54 +0800

    2

---
+++ app/http/binding/User.go
@@ -0,0 +1,72 @@
+package binding
+
+import (
+	"go-app/app/code"
+	"go-app/config"
+	"go-app/lib/httpclient"
+	"go-app/lib/response"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var User = new(userBinding)
+
+type userBinding struct{}
+
+type UserLoginParams struct {
+	Username       string `form:"username" binding:"required" label:"帐号"`
+	Password       string `form:"password" binding:"required" label:"密码"`
+	TurnstileToken string `form:"turnstileToken" binding:"required" label:"TurnstileToken"`
+}
+
+func (u userBinding) Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var data UserLoginParams
+		params, err := bindParams(c, data)
+		if err != nil {
+			c.Abort()
+			return
+		}
+		c.Set("UserLoginParams", params)
+		c.Set("TurnstileToken", params.TurnstileToken)
+		c.Next()
+	}
+}
+
+func (u userBinding) VerifyTurnstile() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		payload := map[string]string{
+			"secret":   config.CLOUDFLARE.TurnstileSecret,
+			"response": c.MustGet("TurnstileToken").(string),
+		}
+		url := config.CLOUDFLARE.TurnstileUrl
+		var result = struct {
+			Success bool `json:"success,omitempty"`
+		}{}
+		httpclient.C().R().SetResult(&result).SetFormData(payload).Post(url)
+		if !result.Success {
+			c.JSON(http.StatusUnauthorized, response.NewJson().Error(code.NewError(-1, "未通过人机验证")))
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
+type RefreshTokenParams struct {
+	RefreshToken string `form:"refreshToken" binding:"required" label:"Token"`
+}
+
+func (userBinding) RefreshToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var data RefreshTokenParams
+		params, err := bindParams(c, data)
+		if err != nil {
+			c.Abort()
+			return
+		}
+		c.Set("RefreshTokenParams", params)
+		c.Next()
+	}
+}
